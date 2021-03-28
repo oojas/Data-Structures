8a8d1227044262c4c72d4583fb0d5a4ca690a4bb
oojas <oojasgupta@gmail.com>
2021-03-28 16:23:39 +0530

    Added linked list and array questions

---
+++ LinkedList/implementationofLinkedList.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class implementationofLinkedList {
+  class node{
+      int data;
+      node next;
+      node(int data){
+          this.data=data;
+      }
+
+  }
+  node head;
+  void add(int data){
+     node toAdd=new node(data);
+     if(isEmpty()){ 
+         head=toAdd;
+     }
+     else{
+         node temp=head;
+         while(temp.next!=null){
+             temp=temp.next;
+         }
+         temp.next=toAdd;
+         toAdd.next=null;
+     }
+  }
+  void print(){
+      node temp=head;
+      while(temp.next!=null){
+          System.out.print(temp.data+" ");
+          temp=temp.next;
+      }
+  }
+  Boolean isEmpty(){
+      return head==null;
+  }
+  public static void main(String s[]){
+      int n;
+      implementationofLinkedList obj=new implementationofLinkedList();
+      Scanner sc=new Scanner(System.in);
+      n=sc.nextInt();
+   System.out.println("Enter the data to add"); 
+     for(int i=0 ; i<n ; i++){
+          obj.add(sc.nextInt());
+     }
+     obj.print();     
+     sc.close();
+  }
+
+}
