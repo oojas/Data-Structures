6aae6be30d12f2819f93b1a054376706b32a55c4
oojas <oojasgupta@gmail.com>
2021-04-10 15:49:15 +0530

    Balancing paranthesis

---
+++ Stack/BalancedParanthesis.java
@@ -0,0 +1,67 @@
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BalancedParanthesis {
+   public static void main(String a[])
+   {
+   ArrayDeque<Character> s=new ArrayDeque<Character>();
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the string ");
+    String b=sc.next();
+    sc.close();
+    boolean val=true;
+     for(Character c: b.toCharArray())
+     {
+         if(c=='(' || c=='{' || c=='[')
+         {
+             s.push(c);
+         }
+         else if(c==')')
+         {
+             if(s.peek()!='(')
+             {
+                 val=false;
+                 break;
+             }
+             else
+             {
+                 s.pop();
+             }
+         }
+         else if(c==']')
+         {
+             if(s.peek()!='[')
+             {
+                 val=false;
+                 break;
+             }
+             else
+             {
+                 s.pop();
+             }
+         }
+         else if(c=='}')
+         {
+             if(s.peek()!='{')
+             {
+                 val=false;
+                 break;
+             }
+             else
+             {
+                 s.pop();
+             }
+         }
+     }
+     if(val==false || s.size()!=0)
+     {
+         System.out.println("NO");
+     }
+     else
+     {
+         System.out.println("YES");
+     }
+   }    
+}
