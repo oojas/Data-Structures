e09b14aa4ed6b9304eb6d777b40d4db1f7aa9936
oojas <oojasgupta@gmail.com>
2021-04-29 01:48:28 +0530

    Adding one more question

---
+++ Recursion/sumOfdigits.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class sumOfdigits {
+    public static void main(String s[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        sc.close();
+        int val=sum(n);
+        System.out.println(val);
+
+    }
+    public static int sum(int n)
+    {
+        if(n==0)return 0;
+        int val=n%10+sum(n/10);
+        return val;
+    }
+}
